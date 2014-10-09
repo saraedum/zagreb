23adb1d6b093109bf271afc74ac7fedca80210ab
Julian Rueth <julian.rueth@fsfe.org>
2014-10-09 14:17:04 +0200

    CentralDist now starts in the center

--- module/dist/central_dist.h
+++ module/dist/central_dist.h
@@ -5,12 +5,16 @@
 #include "../../util/assert.h"
 
 class CentralDist : public Dist {
+	private:
+		uint32_t offset;
 	public:
-		CentralDist(const uint32_t width, const uint32_t height):Dist(width,height){
+		CentralDist(const uint32_t width, const uint32_t height):Dist(width,height),offset(width*width+height*height){
 			compute_max_dist();
 		}
 		virtual uint32_t dist(const uint8_t x, const uint8_t y){
-			return width*2 - x + height*2 - y;
+			int16_t x_ = width - 1 - (int16_t)2*x;
+			int16_t y_ = height - 1 - (int16_t)2*y;
+			return offset - x_*x_ - y_*y_;
 		}
 };
 
