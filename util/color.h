f1b56bc14b46530470d24ce0603be38897911836
Julian Rueth <julian.rueth@fsfe.org>
2014-10-09 13:47:45 +0200

    Fixed a division in Color

--- util/color.h
+++ util/color.h
@@ -7,9 +7,9 @@ class Color{
 		Color(const uint8_t r, const uint8_t g, const uint8_t b):r(r),g(g),b(b){}
 		Color(const uint32_t c):r((c>>16)&0xff),g((c>>8)&0xff),b(c&0xff){}
 		void scale(const uint8_t brightness){
-			r = ((uint16_t)r*(uint16_t)brightness)/255;
-			g = ((uint16_t)g*(uint16_t)brightness)/255;
-			b = ((uint16_t)b*(uint16_t)brightness)/255;
+			r = ((uint16_t)r*(uint16_t)brightness)>>8;
+			g = ((uint16_t)g*(uint16_t)brightness)>>8;
+			b = ((uint16_t)b*(uint16_t)brightness)>>8;
 		}
 		uint32_t color(){
 			return (((uint32_t)r)<<16) | (((uint32_t)g)<<8) | (uint32_t)b;
