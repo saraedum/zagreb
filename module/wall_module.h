580a42576231d59a72c9d63fa478f87978bfb859
Julian Rueth <julian.rueth@fsfe.org>
2014-10-26 17:47:46 +0100

    display stats and text

--- module/wall_module.h
+++ module/wall_module.h
@@ -4,6 +4,59 @@
 #include "delay_module.h"
 #include "fps.h"
 
+inline bool draw_stats(uint8_t x, uint8_t y);
+
+uint8_t TEXT[][10] = {
+	{
+	 0b11111111,
+	 0b11111111,
+	 0b11000000,
+	 0b11000000,
+	 0b11111111,
+	 0b11111111,
+	 0b00000011,
+	 0b00000011,
+	 0b11111111,
+	 0b11111111,
+	},
+	{
+	 0b11111111,
+	 0b11111111,
+	 0b11000011,
+	 0b11000011,
+	 0b11000011,
+	 0b11000011,
+	 0b11000011,
+	 0b11000011,
+	 0b11111111,
+	 0b11111111,
+	},
+	{
+	 0b11000011,
+	 0b11000011,
+	 0b11000011,
+	 0b11000011,
+	 0b11000011,
+	 0b11000011,
+	 0b11000011,
+	 0b11000011,
+	 0b11111111,
+	 0b11111111,
+	},
+	{
+	 0b11000000,
+	 0b11000000,
+	 0b11000000,
+	 0b11000000,
+	 0b11000000,
+	 0b11000000,
+	 0b11000000,
+	 0b11000000,
+	 0b11111111,
+	 0b11111111,
+	},
+};
+
 template <typename DerivedModule>
 class WallModule : public DelayModule {
 	protected:
@@ -13,6 +66,34 @@ class WallModule : public DelayModule {
 		uint32_t last;
 		FPS fps;
 		const uint8_t delay=HZ(60);
+
+		inline bool pre(uint8_t x, uint8_t y){
+			if (y==0 && x < MODES){
+				if (abs(now() - STATS_UNTIL) < 1000){
+					Color c = Color(0,0,0);
+					if (x==BRIGHTNESS_MODE)
+						c.r = 255;
+					if (x==BRIGHTNESS_DECAY)
+						c.g = 255;
+					if (x==mode() || is_cycle())
+						c.b = 255;
+					wall->set(x, y, c);
+					return 1;
+				}
+			}
+			return 0;
+		}
+
+		inline void post(uint8_t x, uint8_t y){
+			uint8_t c = mbeat()/1000%360;
+			if (c < 4){
+				if (!(TEXT[c][y]&(128>>x))){
+					Color c = wall->get(x,y);
+					wall->set(x,y,Color(c.r>>6,c.g>>6,c.b>>6));
+				}
+			}
+		}
+
 	public:
 		WallModule(Wall* const wall):wall(wall),last(now()){}
 		virtual uint16_t loop_delayed(){
@@ -29,7 +110,11 @@ class WallModule : public DelayModule {
 					return 0;
 				return last + delay - now();
 			}else if(wall->has(x,y)){
-				static_cast<DerivedModule*>(this)->draw();
+				if (pre(x,y)){}
+				else{
+					static_cast<DerivedModule*>(this)->draw();
+					post(x,y);
+				}
 			}
 			x++;
 			return 0;
