c0052268a8775b7bde9d12db260d3a2c73107df1
Julian Rueth <julian.rueth@fsfe.org>
2014-10-26 17:52:10 +0100

    yacin's config

--- zagreb.ino
+++ zagreb.ino
@@ -42,7 +42,7 @@ uint8_t mode(){
 	return MODE&MODE_MASK;
 }
 
-Wall wall(13, 10);
+Wall wall(8, 8);
 
 #include "module/sound_energy.h"
 SoundEnergy sound_energy;
@@ -52,68 +52,45 @@ SoundBrightness sound_brightness(sound_energy);
 #include "module/linear_rotary_encoder.h"
 #include "module/control.h"
 
+
 const uint8_t id3[] PROGMEM = {
- 15, 14,  5,  4,
- 16, 13,  6,  3,
- 17, 12,  7,  2,
- 18, 11,  8,  1,
- 19, 10,  9,  0
+ 3, 4,  11,  12,
+ 2, 5,  10,  13,
+ 1, 6,  9,  14,
+ 0, 7,  8,  15
 };
-Adafruit_NeoPixel* const strip3 = new Adafruit_NeoPixel(20, 3, NEO_GRB + NEO_KHZ800);
-ProgmemBoard* const board3 = new ProgmemBoard(strip3, 4, 5, id3);
+Adafruit_NeoPixel* const strip3 = new Adafruit_NeoPixel(16, 3, NEO_GRB + NEO_KHZ800);
+ProgmemBoard* const board3 = new ProgmemBoard(strip3, 4, 4, id3);
 
-const uint8_t id5[] PROGMEM = { 
-  4, 5, 14, 15,
-  3, 6, 13, 16,
-  2, 7, 12, 17,
-  1, 8, 11, 18,
-  0, 9, 10, 19
+const uint8_t id5[] PROGMEM = {
+ 15, 14,  13,  12,
+ 8, 9,  10,  11,
+ 7, 6,  5,  4,
+ 0, 1,  2,  3
 };
-Adafruit_NeoPixel* const strip5 = new Adafruit_NeoPixel(20, 5, NEO_GRB + NEO_KHZ800);
-ProgmemBoard* const board5 = new ProgmemBoard(strip5, 4, 5, id5);
+Adafruit_NeoPixel* const strip5 = new Adafruit_NeoPixel(16, 5, NEO_GRB + NEO_KHZ800);
+ProgmemBoard* const board5 = new ProgmemBoard(strip5, 4, 4, id5);
 
-const uint8_t id6[] PROGMEM = {
- 19, 10,  9,  0,
- 18, 11,  8,  1,
- 17, 12,  7,  2,
- 16, 13,  6,  3,
- 15, 14,  5,  4
-};
-Adafruit_NeoPixel* const strip6 = new Adafruit_NeoPixel(20, 6, NEO_GRB + NEO_KHZ800);
-ProgmemBoard* const board6 = new ProgmemBoard(strip6, 4, 5, id6);
 
-const uint8_t id9[] PROGMEM = {
- 18, 17, 16, 15,
- 11, 12, 13, 14,
- 10,  9,  8,  7,
-  4,  5,  6, NO_PIXEL,
-  3,  2,  1,  0
+const uint8_t id10[] PROGMEM = {
+ 3, 4,  11,  12,
+ 2, 5,  10,  13,
+ 1, 6,  9,  14,
+ 0, 7,  8,  15
 };
-Adafruit_NeoPixel* const strip9 = new Adafruit_NeoPixel(19, 9, NEO_GRB + NEO_KHZ800);
-ProgmemBoard* const board9 = new ProgmemBoard(strip9, 4, 5, id9);
+Adafruit_NeoPixel* const strip10 = new Adafruit_NeoPixel(16, 10, NEO_GRB + NEO_KHZ800);
+ProgmemBoard* const board10 = new ProgmemBoard(strip10, 4, 4, id10);
 
-const uint8_t id10[] PROGMEM = { 
-  8,  9, 10, 11,
-  7,  6,  5,  4, 
-  0,  1,  2,  3
-};
-Adafruit_NeoPixel* const strip10 = new Adafruit_NeoPixel(12, 10, NEO_GRB + NEO_KHZ800);
-ProgmemBoard* const board10 = new ProgmemBoard(strip10, 4, 3, id10);
 
-const uint8_t id11[] PROGMEM = { 
-  0,
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9
+const uint8_t id11[] PROGMEM = {
+ 12, 11,  4,  3,
+ 13, 10,  5,  2,
+ 14, 9,  6,  1,
+ 15, 8,  7,  0
 };
-Adafruit_NeoPixel* const strip11 = new Adafruit_NeoPixel(10, 11, NEO_GRB + NEO_KHZ800);
-ProgmemBoard* const board11 = new ProgmemBoard(strip11, 1, 10, id11);
+
+Adafruit_NeoPixel* const strip11 = new Adafruit_NeoPixel(16, 11, NEO_GRB + NEO_KHZ800);
+ProgmemBoard* const board11 = new ProgmemBoard(strip11, 4, 4, id11);
 
 Control control;
 LinearRotaryEncoder<uint16_t> bpm_encoder(BPM_PIN_0, BPM_PIN_1, 1, 1000, &BPM, 1);
@@ -121,12 +98,10 @@ LinearRotaryEncoder<uint16_t> bpm_encoder(BPM_PIN_0, BPM_PIN_1, 1, 1000, &BPM, 1
 LinearRotaryEncoder<uint16_t> color_encoder(BRIGHTNESS_PIN0, BRIGHTNESS_PIN1, 128, 512*20, &COLORS512, 128);
 
 void setup() {
-	wall.add( board3,  0,  0);
-	wall.add( board5,  4,  0);
-	wall.add( board6,  0,  5);
-	wall.add( board9,  4,  5);
-	wall.add(board10,  8,  0);
-	wall.add(board11, 12,  0);
+	wall.add( board3,   4,  0);
+	wall.add( board5,   4,  4);
+	wall.add( board10,  0,  0);
+	wall.add( board11,  0,  4);	
 }
 
 void loop() {
